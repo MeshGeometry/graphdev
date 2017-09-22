f7a2659fabe814e193e16c52966277bfdc2e0912
Daniel Hambleton <danie@meshconsultants.ca>
2017-09-21 22:41:35 -0400

    a test

--- Source/Test_GraphDev.cpp
+++ Source/Test_GraphDev.cpp
@@ -18,12 +18,37 @@ void Init()
 	ctx->RegisterSubsystem(fs);
 }
 
+//ok some stuff
+struct GraphItem : public RefCounted
+{
 
-TEST(Base, FileParse)
+public:
+	// Construct
+	GraphItem()
+	{
+	}
+
+	/// Work function. Called with the work item and thread index (0 = main thread) as parameters.
+	void(*workFunction_)(const Variant& in, Variant& out);
+
+};
+
+void Add_Test(const Variant& in, Variant& out)
+{
+	float a = in.GetFloat();
+	out = a + a;
+}
+
+
+TEST(Base, GraphItem)
 {
 	if (!ctx) {
 		Init();
 	}
 
-
+	GraphItem* gi = new GraphItem();
+	gi->workFunction_ = Add_Test;
+	Variant a = 5;
+	Variant b = 6;
+	Add_Test(a, b);
 }
