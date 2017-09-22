97531070594c11fa83977ea8d4df7002386adb5f
Daniel Hambleton <daniel.hambleton@meshconsultants.ca>
2017-09-22 16:40:32 -0400

    little example of hyper edge

--- Source/Test_GraphDev.cpp
+++ Source/Test_GraphDev.cpp
@@ -18,37 +18,109 @@ void Init()
 	ctx->RegisterSubsystem(fs);
 }
 
-//ok some stuff
-struct GraphItem : public RefCounted
+//a link is hyper edge with a function
+struct GraphLink
 {
+	GraphLink() {};
+
+	Vector<String> sources_;
+	Vector<String> targets_;
+
+	void(*function_)(const VariantVector& inputs, VariantVector& outputs);
+};
+
+//graph def
+class GraphEngine : public Object
+{
+	URHO3D_OBJECT(GraphEngine, Object);
 
 public:
-	// Construct
-	GraphItem()
+	GraphEngine(Context* context) : Object(context) {};
+	~GraphEngine() {};
+
+	HashMap<String, Variant> graphValues_;
+	HashMap<String, GraphLink> graphLinks_;
+
+	void AddValue(String uniqueName, Variant value)
 	{
+		graphValues_[uniqueName] = value;
 	}
 
-	/// Work function. Called with the work item and thread index (0 = main thread) as parameters.
-	void(*workFunction_)(const Variant& in, Variant& out);
+	void Solve()
+	{
+		//for now, forget about ordering the solve
+		HashMap<String, GraphLink>::ConstIterator itr;
+		for (itr = graphLinks_.Begin(); itr != graphLinks_.End(); itr++)
+		{
+			//collect the inputs
+			VariantVector inputs;
+			for (int i = 0; i < itr->second_.sources_.Size(); i++)
+			{
+				Variant currentInput = graphValues_[itr->second_.sources_[i]];
+				inputs.Push(currentInput);
+			}
+
+			//collect the outputs
+			VariantVector outputs;
+			for (int i = 0; i < itr->second_.targets_.Size(); i++)
+			{
+				Variant targetOutput = graphValues_[itr->second_.targets_[i]];
+				outputs.Push(targetOutput);
+			}
+
+			//call the work functions
+			itr->second_.function_(inputs, outputs);
+
+			//propagate the results
+			int size = Min(itr->second_.targets_.Size(), outputs.Size());
+			for (int i = 0; i < size; i++)
+			{
+				Variant targetOutput = outputs[i];
+				graphValues_[itr->second_.targets_[i]] = targetOutput;				
+			}
+
+			//done?
+		}
+	}
 
 };
 
-void Add_Test(const Variant& in, Variant& out)
+//special signature function
+void Adder(const VariantVector& in, VariantVector& out)
 {
-	float a = in.GetFloat();
-	out = a + a;
+	float a = 0.0;
+	for (int i = 0; i < in.Size(); i++)
+	{
+		a += in[i].GetFloat();
+	}
+
+	out[0] = a;
 }
 
-
-TEST(Base, GraphItem)
+TEST(Base, Graph)
 {
 	if (!ctx) {
 		Init();
 	}
 
-	GraphItem* gi = new GraphItem();
-	gi->workFunction_ = Add_Test;
-	Variant a = 5;
-	Variant b = 6;
-	Add_Test(a, b);
+	GraphEngine* ge = new GraphEngine(ctx);
+
+	//add some values
+	ge->AddValue("A", 1.0f);
+	ge->AddValue("B", 2.0f);
+	ge->AddValue("C", 0.0f);
+
+	//add a link
+	GraphLink gl;
+	gl.sources_.Push("A");
+	gl.sources_.Push("B");
+	gl.targets_.Push("C");
+	gl.function_ = Adder;
+	ge->graphLinks_["AdderLink"] = gl;
+
+	//solve
+	ge->Solve();
+
+	float result = ge->graphValues_["C"].GetFloat();
+	EXPECT_EQ(result, 3.0f);
 }
